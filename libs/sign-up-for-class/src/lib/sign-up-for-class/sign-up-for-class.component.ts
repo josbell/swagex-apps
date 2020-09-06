aec615765279f90da7bcc283e080b472a4cc06df
Josbell Quiros <josbell.quiros@oasisdigital.com>
2020-09-06 19:28:27 -0400

    Unsubscribe from subscriptions on destroy

--- libs/sign-up-for-class/src/lib/sign-up-for-class/sign-up-for-class.component.ts
+++ libs/sign-up-for-class/src/lib/sign-up-for-class/sign-up-for-class.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { FloorSpotSelectionComponent } from '../floor-spot-selection/floor-spot-selection.component';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { SelectClassDateTimeComponent } from '../select-class-date-time/select-class-date-time.component';
-
+import { filter, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 @Component({
   selector: 'swagex-sign-up-for-class',
   templateUrl: './sign-up-for-class.component.html',
   styleUrls: ['./sign-up-for-class.component.scss']
 })
-export class SignUpForClassComponent implements OnInit {
+export class SignUpForClassComponent implements OnInit, OnDestroy {
+  private _unsubscribe: Subject<void> = new Subject();
   constructor(private _bottomSheet: MatBottomSheet, public dialog: MatDialog) {}
 
   ngOnInit(): void {}
@@ -22,10 +24,16 @@ export class SignUpForClassComponent implements OnInit {
       }
     );
 
-    bottomSheetRef.afterDismissed().subscribe(classId => {
-      console.log(`bottomSheetRef classId ${classId}`);
-      this.openDialog(classId);
-    });
+    bottomSheetRef
+      .afterDismissed()
+      .pipe(
+        filter(val => !!val),
+        takeUntil(this._unsubscribe)
+      )
+      .subscribe(classId => {
+        console.log(`bottomSheetRef classId ${classId}`);
+        this.openDialog(classId);
+      });
   }
 
   openDialog(classId: string) {
@@ -33,8 +41,18 @@ export class SignUpForClassComponent implements OnInit {
       data: classId
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
+    dialogRef
+      .afterClosed()
+      .pipe(
+        filter(val => !!val),
+        takeUntil(this._unsubscribe)
+      )
+      .subscribe(result => {
+        console.log(`Dialog result: ${result}`);
+      });
+  }
+
+  ngOnDestroy() {
+    this._unsubscribe.next();
   }
 }
