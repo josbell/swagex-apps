99ebc45806f23424506a5705b8c8a1a6c448b896
Josbell Quiros <josbell.quiros@oasisdigital.com>
2020-10-20 15:40:53 -0400

    Redirect after payment failure

--- apps/eys-studio-web/src/app/app-routing.module.ts
+++ apps/eys-studio-web/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from 'libs/latin-swag-studio-web/home/src/lib/home/home.component';
 import { SignUpForClassComponent } from 'libs/sign-up-for-class/src/lib/sign-up-for-class/sign-up-for-class.component';
 import { PaymentComponent } from 'libs/payment/src/lib/payment/payment.component';
-import { PaymentFailedComponent } from 'libs/payment/src/lib/payment-failed/payment-failed.component';
 import { PaymentSucceededComponent } from 'libs/payment/src/lib/payment-succeeded/payment-succeeded.component';
 import { BookClassSpotsComponent } from '@swagex/sign-up-for-class';
 
@@ -12,7 +11,7 @@ const routes: Routes = [
   { path: 'classes', component: SignUpForClassComponent },
   { path: 'classes/:id/book', component: BookClassSpotsComponent },
   { path: 'payment', component: PaymentComponent },
-  { path: 'payment-failed', component: PaymentFailedComponent },
+  { path: 'payment-failed', redirectTo: 'classes', pathMatch: 'full' },
   { path: 'payment-succeeded', component: PaymentSucceededComponent },
 
   { path: '', redirectTo: 'home', pathMatch: 'full' }
