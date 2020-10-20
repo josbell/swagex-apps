b3c8ba8ecf73d362c44b7f2b0a5ffbeb95a44fc4
Josbell Quiros <josbell.quiros@oasisdigital.com>
2020-10-20 15:12:21 -0400

    Add mobile payment success view

--- libs/payment/src/lib/payment-succeeded/payment-succeeded.component.ts
+++ libs/payment/src/lib/payment-succeeded/payment-succeeded.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'swagex-payment-succeeded',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment-succeeded.component.scss']
 })
 export class PaymentSucceededComponent implements OnInit {
+  constructor(public router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onDone(): void {
+    this.router.navigate(['classes']);
   }
-
 }
